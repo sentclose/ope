0a5c642368ff00ceda0bd49eb1696976b0321a89
Jörn Heinemann <joernheinemann@gmx.de>
2023-07-15 19:22:39 +0200

    added test for all possible values

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,40 @@ pub fn get_ope(key: &OpeKey) -> Ope
 {
 	Ope::new(key, DOMAIN, RANGE)
 }
+
+#[cfg(test)]
+mod tests
+{
+	use alloc::borrow::ToOwned;
+	use alloc::vec::Vec;
+
+	use super::*;
+
+	const MAX_SIZE: u64 = 65531;
+
+	#[test]
+	#[ignore]
+	fn test_all_numbers()
+	{
+		let k = b"this is a key 10".to_owned();
+
+		let ope = get_ope(&k);
+
+		let mut v = Vec::with_capacity(MAX_SIZE as usize);
+
+		//encrypt
+		for i in 23..MAX_SIZE {
+			let en = ope.encrypt(i).unwrap();
+			v.push(en);
+		}
+
+		//check
+		let mut past_item = 0;
+
+		for item in v {
+			assert!(past_item < item);
+
+			past_item = item;
+		}
+	}
+}
